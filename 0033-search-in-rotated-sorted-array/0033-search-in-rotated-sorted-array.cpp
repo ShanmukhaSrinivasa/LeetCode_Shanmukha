8f7276bc6a80f27f976f05f58e79fff75c050d97
Shanmukha Srinivasa <shk.shanmukha360@gmail.com>
2025-09-06 17:28:19 +0530

    Time: 0 ms (100%), Space: 15.3 MB (33.06%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,62 +1,44 @@
-int findPivot(vector<int>&nums)
-{
-    int start = 0;
-    int end = nums.size() - 1;
-
-    int mid = start + (end - start)/2;
-
-    while(start < end)
-    {
-        if(nums[mid] >= nums[0])
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid;
-        }
-        mid = start + (end - start)/2;
-    }
-    return start;
-}
-
-int binarySearch(vector<int> &nums, int s, int e, int key)
-{
-    int start = s;
-    int end = e;
-
-    int mid = start + (end - start)/2;
-
-    while(start <= end)
-    {
-        if(nums[mid] == key)
-        {
-            return mid;
-        }
-        else if(key > nums[mid])
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
-        mid = start + (end - start)/2;
-    }
-    return -1;
-}
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int pivot = findPivot(nums);
-        if(target >= nums[pivot] && target <= nums[nums.size()-1])
+    int n = nums.size();
+    int low = 0 ;
+    int high = n-1;
+
+    while(low <= high)
+    {
+        int mid = (low+high)/2;
+        if(nums[mid] == target)
         {
-            return binarySearch(nums, pivot, nums.size()-1, target);
+            return mid;
         }
+
+        //left sorted
+        if(nums[low] <= nums[mid])
+        {
+            if(nums[low] <= target &&  target <= nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        //right sorted
         else
         {
-            return binarySearch(nums, 0, pivot-1, target);
+            if(nums[mid] <= target && target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
         }
+    }
+    return -1;
     }
 };
